dfb34d43a18721d15499a7c0e0a1a354d8281855
Gehan Pasindhu <pasindhu97@gmail.com>
2022-10-08 01:16:40 +0530

    RFiles

---
+++ SimpleRLoop/ForLoop/FLoopWithBreak.r
@@ -0,0 +1,10 @@
+name <- list("Anna","John","Randy","Spencer")
+
+for(names in name){
+    if(names == "John"){
+        break
+    }
+    print(names)
+}
+
+#output : Anna
---
+++ SimpleRLoop/ForLoop/RLoopThroughalist.r
@@ -0,0 +1,7 @@
+games <- list("cricket","football","hockey","swiming")
+
+for(games in games){
+    print (games)
+}
+
+#output : cricket,football,hockey,swiming
